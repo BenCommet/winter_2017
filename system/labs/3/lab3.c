6d204841df384117912e9699fe785769c07c0d05
Ben Commet <bencommet@gmail.com>
2017-02-06 22:34:58 -0500

    more progress in lab 3

--- system/labs/3/lab3.c
+++ system/labs/3/lab3.c
@@ -3,23 +3,24 @@
 
 #define LEN 4
 
-//char* strnsub (char *p, int n);
+char* strnsub (char *p, int n);
 
-int main()
-{
+int main(){
 	char line[] = "His textbook was bought from that bookstore";  
 	char *p1, *p2;
-
+	int counter = 1;
 	p1 = line;
 	//printf("%d", counter);
 	while (*p1) 	
 	{
-		
-                *p2 = p1[1];
+                char* feesh = strnsub(p1, 4);
+		p2 = &line[counter];
 		while (*p2){
-			*p2 = p2[1];
+			
+			*(p2 ++);
 		}
 		*p1++;
+		counter ++;	
 
 //done:	//printf ("the first substring: %s\n", strnsub(p1, LEN));
 	//printf ("the second substring: %s\n", strnsub(p2, LEN));
@@ -31,8 +32,14 @@ int main()
 
 // returns a string with the first n characters of string p
 
-//char* strnsub (char *p, int n)
-//{
-	// write function definition here
-
-//}
+char* strnsub (char *p, int n){
+	char returnString[n];
+	int i; 
+	for(i = 0; i < 4; i++){
+		if(*(p + i)){
+			returnString[i] = *(p + i);
+		}
+	}
+	printf("%s\n", returnString);
+	return "";
+}
